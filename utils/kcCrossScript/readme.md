0df03a5a30a2f4e98d8b5b634ebfc3327eafa521
Sean Casey <4674433+CaseyLabs@users.noreply.github.com>
2026-04-13 10:01:31 -0700

    Update kcCrossScript

--- utils/kcCrossScript/readme.md
+++ utils/kcCrossScript/readme.md
@@ -1,19 +1,24 @@
-# kcCrossShell
-
+# kcCrossScript
 
 ## Overview
 
 A simple cross-platform script template that will run in any Linux/MacOS terminal shell (`sh/bash/zsh`) or in Windows PowerShell.
 
-Inspired by Jeff Hykin's comment on Stackoverflow: https://stackoverflow.com/a/67292076
+Inspired by Jeff Hykin's comment on Stack Overflow: https://stackoverflow.com/a/67292076
 
 This script can be used to run any command that is not OS or shell specific (such as commands like "npm install", "go build", "docker build", etc).
 
-This allows for the creation of a single build script that can be distributed and ran on any Linux, MacOS, or Windows host.
+This allows for the creation of a single build script that can be distributed and run on any Linux, MacOS, or Windows host.
+
+## Why?
+
+Why not? 😆 Honestly, I just wanted to prove to myself it could be done. This is not a real-world production workflow recommendation.
 
 ## Setup
 
-- Git clone this repo: `git clone --depth 1 https://github.com/CaseyLabs/kcCrossShell.git`
+- Git clone this repo: `git clone --depth 1 https://github.com/CaseyLabs/kcUtils.git`
+
+- Go to this utility: `cd kcUtils/kcCrossScript`
 
 - Edit `xshell.sh.ps1`
 
@@ -21,33 +26,31 @@ This allows for the creation of a single build script that can be distributed an
 
 - Add your build commands there. For example:
 
-```
+```shell
 # BEGIN_SCRIPT_COMMANDS
 # --------------------------------------------------------
 
 echo "this is my first command example"
 echo "this is my second command example"
 
-# Below are examples of other commands that could be used:
-# npm install
-# go build
-# docker build .
-
 # --------------------------------------------------------
 END_SCRIPT_COMMANDS
-
 ```
 
 ## Usage
 
 Run the script in a Terminal or PowerShell session.
 
-**Terminal (Linux/MacOS):** `./xshell.sh.ps1`
+**Terminal (Linux/MacOS):** 
+
+```shell
+./xshell.sh.ps1
+```
 
 Example output:
 
-```
-Running zsh shell script...
+```shell
+Running shell script...
 
 this is my first command example
 this is my second command example
@@ -55,11 +58,15 @@ this is my second command example
 Finished running shell script.
 ```
 
-**PowerShell (Windows):** `.\xshell.sh.ps1`
+**PowerShell (Windows):** 
+
+```powershell
+.\xshell.sh.ps1
+```
 
 Example output:
 
-```
+```powershell
 Running PowerShell script...
 
 this is my first command example
--- utils/kcCrossScript/xshell.sh.ps1
+++ utils/kcCrossScript/xshell.sh.ps1
@@ -1,23 +1,25 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env sh
 
-## crossShell 
-## ----------
-## A cross-platform script template that will run in any Linux/MacOS Terminal (sh/bash/zsh) or in Windows PowerShell.
-##
-## Inspired by Jeff Hykin on Stackoverflow: https://stackoverflow.com/a/67292076
-##
-## This script can be used to run any command that is not OS or shell specific (such as commands like "npm install", "go build", "docker build", etc).
-## This allows for a single build script that can be ran on any Linux, MacOS, or Windows host.
-##
-## Add your commands to the "BEGIN_SCRIPT_COMMANDS" section below.
+# kcCrossScript
+# -------------
+# A cross-platform script template that will run in any Linux/MacOS Terminal shell, or in Windows PowerShell.
+#
+# Inspired by Jeff Hykin on Stack Overflow: https://stackoverflow.com/a/67292076
+#
+# This script can be used to run any command that is not OS or shell specific (such as commands like "npm install", "go build", "docker build", etc).
+#
+# This allows for a single build script that can be run on any Linux, MacOS, or Windows host.
+#
+# Add your commands to the "BEGIN_SCRIPT_COMMANDS" section below.
 
 echo --% >/dev/null;: ' | out-null
 
 <#'
 : <<'END_SCRIPT_COMMANDS'
 
+## --- Add your end-user script commands here ---
 # BEGIN_SCRIPT_COMMANDS
-# --------------------------------------------------------
+# ---------------------
 
 echo "this is my first command example"
 echo "this is my second command example"
@@ -27,52 +29,69 @@ echo "this is my second command example"
 # go build
 # docker build .
 
-# --------------------------------------------------------
+# ---------------------
 END_SCRIPT_COMMANDS
 
-# --- Shell function (bash/zsh/etc)
+# --- Do not edit below this line ---
 
-shell_name=$(basename "$SHELL")
-echo "Running $shell_name shell script..."
+# POSIX Shell Parsing
+echo "Running Shell script..."
 echo ""
 
 # Read commands between BEGIN_SCRIPT_COMMANDS and END_SCRIPT_COMMANDS markers
-commands=$(sed -n '/^: <<'"'"'END_SCRIPT_COMMANDS'"'"'$/,/^END_SCRIPT_COMMANDS$/p' "$0" | sed '1d;$d')
-while IFS= read -r line; do
-  if [ -n "$line" ]; then
-    eval "$line"
-  fi
-done <<< "$commands"
+sed -n '/^: <<'"'"'END_SCRIPT_COMMANDS'"'"'$/,/^END_SCRIPT_COMMANDS$/p' "$0" | sed '1d;$d' | while IFS= read -r line; do
+	if [ -n "$line" ] && [ "${line#\#}" = "$line" ]; then
+		eval "$line"
+		command_status=$?
+		if [ "$command_status" -ne 0 ]; then
+			exit "$command_status"
+		fi
+	fi
+done
+command_status=$?
+if [ "$command_status" -ne 0 ]; then
+	exit "$command_status"
+fi
 
 echo ""
 echo "Finished running shell script."
 
-exit 0 #>
+exit 0
+# shellcheck disable=SC2317
+: <<'POWERSHELL_SCRIPT'
+#>
 
-# --- PowerShell function
+# --- PowerShell Script Parsing
 
 echo "Running PowerShell script..."
 echo ""
 
+$ErrorActionPreference = 'Stop'
 $scriptLines = Get-Content -Path $PSCommandPath
-$commands = @()
+$scriptCommands = @()
 $found = $false
 foreach ($line in $scriptLines) {
   if ($line -eq 'END_SCRIPT_COMMANDS') {
     break
   }
   if ($found -and $line -match '\S') {
-    $commands += $line
+    if (-not $line.TrimStart().StartsWith('#')) {
+      $scriptCommands += $line
+    }
   }
   if ($line -eq ": <<'END_SCRIPT_COMMANDS'") {
     $found = $true
   }
 }
-foreach ($cmd in $commands) {
+foreach ($cmd in $scriptCommands) {
   Invoke-Expression $cmd
+  if ($null -ne $LASTEXITCODE -and $LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+  }
 }
 
 echo ""
 echo "Finished running PowerShell script."
 
 exit
+POWERSHELL_SCRIPT
